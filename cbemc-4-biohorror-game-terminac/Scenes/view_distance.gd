1e490c32954129252efc29760fc3d45f5a9ee054
AlaineMagz <alaine_magdaluyo@dlsu.edu.ph>
2026-01-19 12:25:32 +0800

    Initial Prototype Commit

---
+++ cbemc-4-biohorror-game-terminac/Scenes/view_distance.gd
@@ -0,0 +1,11 @@
+extends CSGCombiner3D
+
+@export var geometry : GeometryInstance3D
+@export var player : CharacterBody3D
+
+@export var height_distance_mult = 1
+
+func _process(_delta: float) -> void:
+	var mat = geometry.material_override
+	mat.set("distance_fade_max_distance", max(5,5 + (player.position.y * height_distance_mult)))
+	mat.set("distance_fade_min_distance", max(10,10 + (player.position.y * height_distance_mult)))
